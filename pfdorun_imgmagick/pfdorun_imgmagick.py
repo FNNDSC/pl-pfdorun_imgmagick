ec9987f0ddc263d28360171c9b9eb549dad184b7
Arushi Vyas <arushi@havana.lan>
2020-09-02 12:30:48 -0400

    implemented run() and show_man_page()

--- pfdorun_imgmagick/pfdorun_imgmagick.py
+++ pfdorun_imgmagick/pfdorun_imgmagick.py
@@ -16,77 +16,20 @@ sys.path.append(os.path.dirname(__file__))
 
 # import the Chris app superclass
 from chrisapp.base import ChrisApp
-
+from pfdo_run      import pfdo_run
 
 Gstr_title = """
 
-Generate a title from 
-http://patorjk.com/software/taag/#p=display&f=Doom&t=pfdorun_imgmagick
+       _               __    _                          _                                       _      _    
+      | |             / _|  | |                        (_)                                     (_)    | |   
+ _ __ | |______ _ __ | |_ __| | ___  _ __ _   _ _ __    _ _ __ ___   __ _ _ __ ___   __ _  __ _ _  ___| | __
+| '_ \| |______| '_ \|  _/ _` |/ _ \| '__| | | | '_ \  | | '_ ` _ \ / _` | '_ ` _ \ / _` |/ _` | |/ __| |/ /
+| |_) | |      | |_) | || (_| | (_) | |  | |_| | | | | | | | | | | | (_| | | | | | | (_| | (_| | | (__|   < 
+| .__/|_|      | .__/|_| \__,_|\___/|_|   \__,_|_| |_| |_|_| |_| |_|\__, |_| |_| |_|\__,_|\__, |_|\___|_|\_\\
+| |            | |                                 ______            __/ |                 __/ |            
+|_|            |_|                                |______|          |___/                 |___/             
 
 """
-
-Gstr_synopsis = """
-
-(Edit this in-line help for app specifics. At a minimum, the 
-flags below are supported -- in the case of DS apps, both
-positional arguments <inputDir> and <outputDir>; for FS apps
-only <outputDir> -- and similarly for <in> <out> directories
-where necessary.)
-
-    NAME
-
-       pfdorun_imgmagick.py 
-
-    SYNOPSIS
-
-        python pfdorun_imgmagick.py                                         \\
-            [-h] [--help]                                               \\
-            [--json]                                                    \\
-            [--man]                                                     \\
-            [--meta]                                                    \\
-            [--savejson <DIR>]                                          \\
-            [-v <level>] [--verbosity <level>]                          \\
-            [--version]                                                 \\
-            <inputDir>                                                  \\
-            <outputDir> 
-
-    BRIEF EXAMPLE
-
-        * Bare bones execution
-
-            mkdir in out && chmod 777 out
-            python pfdorun_imgmagick.py   \\
-                                in    out
-
-    DESCRIPTION
-
-        `pfdorun_imgmagick.py` ...
-
-    ARGS
-
-        [-h] [--help]
-        If specified, show help message and exit.
-        
-        [--json]
-        If specified, show json representation of app and exit.
-        
-        [--man]
-        If specified, print (this) man page and exit.
-
-        [--meta]
-        If specified, print plugin meta data and exit.
-        
-        [--savejson <DIR>] 
-        If specified, save json representation file to DIR and exit. 
-        
-        [-v <level>] [--verbosity <level>]
-        Verbosity level for app. Not used currently.
-        
-        [--version]
-        If specified, print version number and exit. 
-
-"""
-
 
 class Pfdorun_imgmagick(ChrisApp):
     """
@@ -132,6 +75,182 @@ class Pfdorun_imgmagick(ChrisApp):
         Use self.add_argument to specify a new app argument.
         """
 
+        self.add_argument("-i", "--inputFile", help="input file", type=str,
+                          dest='inputFile', optional=False, default="")
+
+        self.add_argument("--exec", help="CLI command to execute", type=str,
+                          dest='exec', optional=False, default="")
+
+        self.add_argument("--analyzeFileIndex", help="file index per directory to analyze", type=str,
+                          dest='analyzeFileIndex', optional=False, default="")
+
+        self.add_argument("--filterExpression", help="string file filter", type=str,
+                          dest='filter', optional=True, default="")
+
+        self.add_argument('--printElapsedTime', dest='printElapsedTime', type=bool, action='store_true',
+                          default=False, optional=True, help='print program run time')
+
+        self.add_argument('--threads', dest='threads', type=str,
+                          default="0", optional=True, help='number of threads for innermost loop processing')
+
+        self.add_argument('--outputLeafDir', dest='outputLeafDir', type=str,
+                          default="", optional=True, help='formatting spec for output leaf directory')
+
+        self.add_argument('--test', dest='test', type=bool, action='store_true',
+                          default=False, optional=True, help='test')
+
+        self.add_argument('--noJobLogging', dest='noJobLogging', type=bool, action='store_true',
+                          default=False, optional=True, help='Turn off per-job logging to file system')
+
+        # self.add_argument('--json', dest='json', type=bool, action='store_true',
+        #                   default=False, optional=True, help='output final return in json')
+
+        self.add_argument('--overwrite', dest='overwrite', type=bool, action='store_true',
+                          default=False, optional=True, help='overwrite files if already existing')
+
+        self.add_argument('--followLinks', dest='followLinks', type=bool, action='store_true',
+                          default=False, optional=True, help='follow symbolic links')
+
+        self.add_argument('-y', '--synopsis', dest='synopsis', type=bool, action='store_true',
+                          default=False, optional=True, help='short synopsis')
+
+    def show_man_page(self, ab_shortOnly=False):
+        """
+        Print the app's man page.
+        """
+        scriptName = os.path.basename(sys.argv[0])
+        shortSynopsis = '''
+
+        NAME
+
+       pfdorun_imgmagick.py 
+
+        SYNOPSIS
+
+            python pfdorun_imgmagick.py                   \\
+                --exec <CLIcmdToExec>                       \\
+                [-i|--inputFile <inputFile>]                \\
+                [--analyzeFileIndex <someIndex>]            \\
+                [--threads <numThreads>]                    \\
+                [--noJobLogging]                            \\
+                [--test]                                    \\
+                [-x|--man]                                  \\
+                [-y|--synopsis]                             \\
+                [--followLinks]                             \\
+                [--json]                                    \\
+                [-v <level>] [--verbosity <level>]          \\
+                [--version]                                 \\
+                <inputDir>                                  \\
+                <outputDir> 
+
+        ''' % scriptName
+
+        description = '''
+        DESCRIPTION
+
+            `pfdorun_imgmagick.py` recursively walks down a directory tree and runs a CLI program
+            from imagemagick (a CLI utility to format images in a desired way)
+            on files in each directory (optionally filtered by some simple
+            expression). Results of each operation are saved in an output tree
+            that preserves the input directory structure.
+
+            ``pl-pfdorun_imgmagick`` runs one of imagemagick's user specified CLI at each path/file location
+            in an input directory, storing results (and logs) at a corresponding 
+            dir location rooted in the output directory.
+
+
+        ARGS
+
+            --exec <CLIcmdToExec>
+            The command line expression to apply at each directory node of the
+            input tree. See the CLI SPECIFICATION section for more information.
+
+            [-i|--inputFile <inputFile>]
+            An optional <inputFile> specified relative to the <inputDir>. If
+            specified, then do not perform a directory walk, but convert only
+            this file.
+
+            [-f|--filterExpression <someFilter>]
+            An optional string to filter the files of interest from the
+            <inputDir> tree.
+
+            [--analyzeFileIndex <someIndex>]
+            An optional string to control which file(s) in a specific directory
+            to which the analysis is applied. The default is "-1" which implies
+            *ALL* files in a given directory. Other valid <someIndex> are:
+
+                'm':   only the "middle" file in the returned file list
+                "f":   only the first file in the returned file list
+                "l":   only the last file in the returned file list
+                "<N>": the file at index N in the file list. If this index
+                    is out of bounds, no analysis is performed.
+
+                "-1" means all files.
+
+            [--outputLeafDir <outputLeafDirFormat>]
+            If specified, will apply the <outputLeafDirFormat> to the output
+            directories containing data. This is useful to blanket describe
+            final output directories with some descriptive text, such as
+            'anon' or 'preview'.
+
+            This is a formatting spec, so
+
+                --outputLeafDir 'preview-%%s'
+
+            where %%s is the original leaf directory node, will prefix each
+            final directory containing output with the text 'preview-' which
+            can be useful in describing some features of the output set.
+
+            [--threads <numThreads>]
+            If specified, break the innermost analysis loop into <numThreads>
+            threads.
+
+            [--noJobLogging]
+            If specified, then suppress the logging of per-job output. Usually
+            each job that is run will have, in the output directory, three
+            additional files:
+
+                    %inputWorkingFile-returncode
+                    %inputWorkingFile-stderr
+                    %inputWorkingFile-stdout
+
+            By specifying this option, the above files are not recorded.
+
+            [-x|--man]
+            Show full help.
+
+            [-y|--synopsis]
+            Show brief help.
+
+            [--json]
+            If specified, output a JSON dump of final return.
+
+            [--followLinks]
+            If specified, follow symbolic links.
+
+            -v|--verbosity <level>
+            Set the app verbosity level.
+
+                0: No internal output;
+                1: Run start / stop output notification;
+                2: As with level '1' but with simpleProgress bar in 'pftree';
+                3: As with level '2' but with list of input dirs/files in 'pftree';
+                5: As with level '3' but with explicit file logging for
+                        - read
+                        - analyze
+                        - write
+            
+            [--version]
+            If specified, print version number and exit. 
+            
+            
+            ''' % (scriptName)
+
+        if ab_shortOnly:
+            return shortSynopsis
+        else:
+            return shortSynopsis + description
+
     def run(self, options):
         """
         Define the code to be run by this plugin app.
@@ -139,11 +258,23 @@ class Pfdorun_imgmagick(ChrisApp):
         print(Gstr_title)
         print('Version: %s' % self.get_version())
 
-    def show_man_page(self):
-        """
-        Print the app's man page.
-        """
-        print(Gstr_synopsis)
+        options.inputDir = options.inputdir
+        options.outputDir = options.outputdir
+
+        pf_do_shell         = pfdo_run.pfdo_run(options)
+
+        # And now run it!
+        # pudb.set_trace()
+        d_pfdo_shell        = pf_do_shell.run(timerStart = True)
+
+        if options.printElapsedTime:
+            pf_do_shell.dp.qprint(
+                    "Elapsed time = %f seconds" %
+                    d_pfdo_shell['runTime']
+            )
+
+        sys.exit(0)
+
 
 
 # ENTRYPOINT
